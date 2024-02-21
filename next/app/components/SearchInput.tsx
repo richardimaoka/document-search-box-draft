c32efa35bfb04612f220356630c9f5b0bd2f4095
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-02-22 08:21:10 +0900

    visual arrangement

--- next/app/components/SearchInput.tsx
+++ next/app/components/SearchInput.tsx
@@ -1,13 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import styles from "./SearchInput.module.css";
 
 interface Props {}
 
 export function SearchInput(props: Props) {
   return (
-    <div>
-      <FontAwesomeIcon icon={faCoffee} /> <input className={styles.component} />
+    <div className={styles.component}>
+      <FontAwesomeIcon className={styles.icon} icon={faMagnifyingGlass} />
+      <input className={styles.input} placeholder="search in documentation" />
     </div>
   );
 }
--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,8 +1,9 @@
+import styles from "./page.module.css";
 import { SearchInput } from "./components/SearchInput";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.component}>
       <SearchInput />
     </div>
   );
