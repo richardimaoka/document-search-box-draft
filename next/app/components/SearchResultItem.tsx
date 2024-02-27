8ea98141d6e10df42cca283fbdebc49b38b989a0
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-02-27 17:52:25 +0900

    link

--- next/app/components/SearchResultItem.tsx
+++ next/app/components/SearchResultItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SearchResultBreadcrumb } from "./SearchResultBreadcrumb";
 import styles from "./SearchResultItem.module.css";
 
@@ -11,10 +12,12 @@ export type SearchResultItemProps = Props;
 
 export function SearchResultItem(props: Props) {
   return (
-    <li className={styles.component}>
-      <div className={styles.title}>{props.title}</div>
-      <SearchResultBreadcrumb breadcrumb={props.breadcrumb} />
-      <div className={styles.description}>{props.description}</div>
-    </li>
+    <Link className={styles.link} href="/documents">
+      <li className={styles.component}>
+        <div className={styles.title}>{props.title}</div>
+        <SearchResultBreadcrumb breadcrumb={props.breadcrumb} />
+        <div className={styles.description}>{props.description}</div>
+      </li>
+    </Link>
   );
 }
